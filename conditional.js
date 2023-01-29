860cead26b1b106c0f96fb2c218a928803884f0b
mahdee18 <mahdeec10@gmail.com>
2023-01-29 16:09:11 +0600

    Basic Knowladge about Conditional

---
+++ conditional.js
@@ -0,0 +1,30 @@
+// Conditional operator
+
+// Greater then >
+// Greater then or eq1ual =>
+// Less then <
+// Less then or equal <=
+//Not equal !=
+//Equal value and equal type ===
+// Not equal value and not equal type !==
+// Allow all conditions &&
+// At least alow single condition conditions ||
+
+// Problem -1
+var myBalance = 100,000;
+if(myBalance>80000){
+    console.log('Congrats , You can Buy Mackbook.!')
+}
+else if(myBalance >60000){
+    console.log('Congrats , You can Buy a Gamming Laptop.!')
+}
+else if(myBalance>40000){
+    console.log('Congrats , You can Buy Lenovo Yoga.!')
+}
+else if(myBalance>20000){
+    console.log('Tumi fokir , tumi furana laptop kino.!')
+}
+else{
+    console.log('Tumi fokir , tumi miskin tumar kopale laptop nai :)')
+}
+
