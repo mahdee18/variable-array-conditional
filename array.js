8d87efc0aeb25d274148c7ce8292e002a13f7001
mahdee18 <mahdeec10@gmail.com>
2023-01-29 12:54:09 +0600

    Basic knowladge of an arrey

---
+++ array.js
@@ -0,0 +1,28 @@
+// Array Declaration
+var arrayName = ['array','element','array-item'];
+
+// Array length
+var arrayNumber = [20,24,45,35,43]
+console.log(arrayName.length)
+
+// Array indexOf
+console.log(arrayName.indexOf(24))
+
+// Array index
+console.log(arrayName[2])
+
+// Array Push
+arrayNumber.push(55)
+console.log(arrayNumber)
+
+// Array POP
+arrayNumber.pop()
+console.log(arrayNumber)
+
+// Push An item on beginning of the array
+arrayNumber.unshift(101)
+console.log(arrayNumber)
+
+// POP An item on beginning of the array
+arrayNumber.shift(101)
+console.log(arrayNumber)
