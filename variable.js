32c253cb4bbcae9ce4979b3cbdd6a5c855bfbd68
mahdee18 <mahdeec10@gmail.com>
2023-01-29 12:26:27 +0600

    Basic knowladge of variable

---
+++ variable.js
@@ -0,0 +1,47 @@
+// Declare a variable
+var name = 'Value';
+
+// Types of variable
+var number = 20;
+var string = 'Mahdee';
+var boolean = true/false ;
+
+// variable operation
+var num1 = 50;
+var num2 = 5;
+
+var sum = num1+num2;
+
+var sub = num1-num2;
+
+var multiplied = num1*num2;
+
+var devided = num1 / num2;
+
+var modulas = num1 % num2;
+
+// Print a Operation
+// console.log(sum,sub,multiplied,devided,modulas);
+
+// Shorthand operator
+var num =10;
+
+ num += 5;
+
+ num -= 5;
+
+ num *= 5;
+
+ num /= 5;
+
+// The increment operator is ++  
+// The decrement operator is --  
+
+// parseInt to convert a string value turn into number.
+var integer = '100';
+var integerConvert = parseInt(integer)
+console.log(integerConvert)
+// parseFloat to convert a string value turn into Floating number.
+var float = '100.4000000';
+var floatConvert = parseFloat(float)
+console.log(floatConvert.toFixed(4))
